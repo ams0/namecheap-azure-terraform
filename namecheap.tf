db389711bf23514e2b84598e01fc83a1958a1bcd
Alessandro Vozza <alessandro.vozza@microsoft.com>
2022-10-18 13:34:04 +0200

    more elegant solution

--- namecheap.tf
+++ namecheap.tf
@@ -2,8 +2,10 @@ resource "namecheap_domain_records" "zone" {
 
   for_each = var.zones
 
-  domain      = each.key
-  mode        = "OVERWRITE"
+  domain = each.key
+  mode   = "OVERWRITE"
   # horrible hack because of https://github.com/namecheap/terraform-provider-namecheap/issues/69
-  nameservers = split(" ", trimsuffix(replace(join(", ", azurerm_dns_zone.zone[each.key].name_servers), ".,", ""), "."))
+  #nameservers = split(" ", trimsuffix(replace(join(", ", azurerm_dns_zone.zone[each.key].name_servers), ".,", ""), "."))
+  # more elegant solution
+  nameservers = [for entry in azurerm_dns_zone.zone[each.key].name_servers : trimsuffix(entry, ".")]
 }
