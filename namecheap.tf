24c4d70f0fa5e80574ef07c175f4c5750c10242d
Alessandro Vozza <alessandro.vozza@microsoft.com>
2022-10-18 12:25:25 +0200

    hack

--- namecheap.tf
+++ namecheap.tf
@@ -2,8 +2,8 @@ resource "namecheap_domain_records" "zone" {
 
   for_each = var.zones
 
-  domain = each.key
-  mode   = "OVERWRITE"
-
-  nameservers = azurerm_dns_zone.zone[each.key].name_servers
+  domain      = each.key
+  mode        = "OVERWRITE"
+  # horrible hack because of https://github.com/namecheap/terraform-provider-namecheap/issues/69
+  nameservers = split(" ", trimsuffix(replace(join(", ", azurerm_dns_zone.zone[each.key].name_servers), ".,", ""), "."))
 }
